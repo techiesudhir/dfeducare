bd6c897ab96ae721ae38cdaedc7b15636213811c
Sudhir <sudhir.bansalk@gmail.com>
2015-12-25 22:22:36 +0530

    sec

---
+++ src/com/dgeducare/beans/admin/SchoolBean.java
@@ -0,0 +1,130 @@
+/**
+ * 
+ */
+package com.dgeducare.beans.admin;
+
+/**
+ * @author Sudhir
+ *
+ */
+public class SchoolBean {
+	String id;
+	String schoolname;
+	String addressline1;
+	String addressline2;
+	String addressline;
+	String city;
+	String state;
+	String pin;
+	String lang;
+	String lat;
+	String website;
+	String typeid;
+	String boardid;
+	String status;
+	String ts;
+	String ip;
+	
+	public SchoolBean() {
+		super();
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getSchoolname() {
+		return schoolname;
+	}
+	public void setSchoolname(String schoolname) {
+		this.schoolname = schoolname;
+	}
+	public String getAddressline1() {
+		return addressline1;
+	}
+	public void setAddressline1(String addressline1) {
+		this.addressline1 = addressline1;
+	}
+	public String getAddressline2() {
+		return addressline2;
+	}
+	public void setAddressline2(String addressline2) {
+		this.addressline2 = addressline2;
+	}
+	public String getAddressline() {
+		return addressline;
+	}
+	public void setAddressline(String addressline) {
+		this.addressline = addressline;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getPin() {
+		return pin;
+	}
+	public void setPin(String pin) {
+		this.pin = pin;
+	}
+	public String getLang() {
+		return lang;
+	}
+	public void setLang(String lang) {
+		this.lang = lang;
+	}
+	public String getLat() {
+		return lat;
+	}
+	public void setLat(String lat) {
+		this.lat = lat;
+	}
+	public String getWebsite() {
+		return website;
+	}
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+	public String getTypeid() {
+		return typeid;
+	}
+	public void setTypeid(String typeid) {
+		this.typeid = typeid;
+	}
+	public String getBoardid() {
+		return boardid;
+	}
+	public void setBoardid(String boardid) {
+		this.boardid = boardid;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getTs() {
+		return ts;
+	}
+	public void setTs(String ts) {
+		this.ts = ts;
+	}
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	
+	
+}
